51604ecf9dec80ea9e6cc38aef4f6d28a9cddaf3
bangitgts <bangitgts@gmail.com>
2021-05-14 13:29:22 +0700

    done get API

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -3,9 +3,33 @@ import { Component } from "react";
 import ProductItem from "../../components/ProductItem/ProductItem";
 import ProductList from "../../components/ProductList/ProductList";
 import { connect } from "react-redux";
+import callApi from "../../utils/apiCaller";
 class ProductListPage extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+  componentDidMount(){
+    // axios({
+    //   method: 'GET',
+    //   url: 'http://localhost:3000/products',
+    //   data: 'null'
+    // })
+    //   .then(res => {
+    //     console.log(res.data)
+    //     this.setState({
+    //       products : res.data
+    //     })
+    //   })
+    //   .catch(err => {console.log(err)});
+    callApi('products','GET',null).then(res => this.setState({
+      products: res.data
+    }))
+  }
   render() {
-    var { products } = this.props;
+    let {products}  = this.state;
     return (
       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <button type="button" className="btn btn-default">
@@ -15,7 +39,6 @@ class ProductListPage extends Component {
       </div>
     );
   }
-
   showProducts(products) {
     var result = null;
     if (products.length > 0) {
